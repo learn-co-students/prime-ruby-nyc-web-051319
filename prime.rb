19bb8482ba2b8f6341ed7731a87e158bccce660c
Constantine Chutis <constantinec84@gmail.com>
2019-04-21 17:43:08 -0400

    Finished.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1
+        return false
+    else
+        for i in 2..(num-1)
+            if (num % i) == 0
+                return false
+            end
+        end
+    end
+    true
+end
