8fa010011195d2548aefb93c231f6f29a233d966
Mary Crivelli <maryecrivelli@gmail.com>
2019-05-02 22:18:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
 # Add  code here!
+def prime?(num)
+  if num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 9 == 0 || num % 10 == 0
+    return false
+  else
+    return true
+end
